5b6c3aaa93ae7f5e89301c31ae1c337f1872bbb9
Ihor Kosheliev <ihor.kosheliev@mev.com>
2023-10-27 21:39:47 +0300

    Remove dotenv dependency from auth test setup
    The dependency dotenv was removed from the auth.test setup since it was no longer necessary. Configurations are now directly imported from a JSON file which simplified the codebase.

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -1,7 +1,4 @@
 import {expect, test as setup, test} from '@playwright/test';
-import * as dotenv from 'dotenv';
-
-dotenv.config();
 const authFile = '.auth/user.json';
 
 setup('authenticate', async ({page}) => {
